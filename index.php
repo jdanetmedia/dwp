df31f1faf4a8c427d25339cb4f209a05af1a084b
Jesper Dalsgaard <post@jdanet.dk>
2018-02-08 10:17:28 +0100

    added materialize framework

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php require_once('includes/header.php') ?>
+<div class="container">
+  <div class="row">
+    <h1>Hello world!</h1>
+  </div>
+</div>
+<?php require_once('includes/footer.php') ?>
