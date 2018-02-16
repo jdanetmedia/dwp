3835e0cbbd9b9db3ff0a457ecdf6954cd690b486
Sebastian Buch <seba5513@easv365.dk>
2018-02-16 12:59:11 +0100

    VIRK NU

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require_once('includes/header.php') ?>
 <div class="container">
   <div class="row">
-    <h1>Hello world!</h1>
+    <h1>Hello world!AFGJKDFJDKFSJSKASDJFK</h1>
   </div>
 </div>
 <?php require_once('includes/footer.php') ?>
