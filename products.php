36057593a23d20056739fa1dacafbb9fa53728bf
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-02-16 10:51:40 +0100

    noUIslider added

---
+++ products.php
@@ -0,0 +1,117 @@
+
+<?php require_once('includes/header.php') ?>
+<div class="container">
+  <div class="row">
+    <div class="input-field col s12 m3">
+      <select>
+        <option value="" disabled selected>Categories</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </div>
+    <div class="col s12 m6">
+      <div id="price-slider"></div>
+    </div>
+    <div class="input-field col s12 m3">
+      <select>
+        <option value="" disabled selected>Sort by</option>
+        <option value="1">Desc. Price</option>
+        <option value="2">Asc. Price</option>
+        <option value="3">Reviews</option>
+        <option value="3">Popularity</option>
+      </select>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/400x400">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-action">
+          <a href="#">$99.95</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php require_once('includes/footer.php') ?>
