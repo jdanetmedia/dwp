d3dc9f070e4af6d3e403fff85d18717abd04a4d2
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-02-21 09:40:14 +0100

    a tag in price changed to p tag

--- cart.php
+++ cart.php
@@ -2,6 +2,24 @@
 
 <div class="container">
   <h1>Cart</h1>
+  <div class="row">
+    <div class="col s12 m12">
+      <div class="card horizontal">
+        <div class="card-image">
+          <img src="http://via.placeholder.com/200x200">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <h5>Product item card.</h5>
+            <p>Product info.</p>
+          </div>
+          <div class="card-action">
+            <p class="price right">$99.95</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php require_once('includes/footer.php') ?>
