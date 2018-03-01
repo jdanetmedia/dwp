996fe31527cd25392a0ebc24d83b8bdd75856a37
Jesper Dalsgaard <post@jdanet.dk>
2018-03-01 18:13:27 +0100

    Hmm, missed a file

--- index.php
+++ index.php
@@ -25,39 +25,6 @@
     </div>
   </div>
   <div class="row">
-    <h4>New producks!</h4>
-    <div class="carousel">
-    <?php
-    function output() {
-      echo "
-      <a class='carousel-item' href='#one!'>
-          <div class='card'>
-            <div class='card-image'>
-              <img src='http://via.placeholder.com/400x400'>
-              <span class='card-title'>Card Title</span>
-            </div>
-            <div class='card-action'>
-              <p class='price'>$99.95</p>
-              <div class='stars right'>
-                <i class='material-icons tiny rated'>star</i>
-                <i class='material-icons tiny rated'>star</i>
-                <i class='material-icons tiny rated'>star</i>
-                <i class='material-icons tiny rated'>star</i>
-                <i class='material-icons tiny'>star_border</i>
-              </div>
-            </div>
-          </div>
-      </a>
-      ";
-    }
-
-    $i = 1;
-    while ($i <= 7) {
-      output();
-      $i++;
-    }
-    ?>
-    </div>
     <h4>Recommended</h4>
     <div class="carousel">
     <?php
@@ -69,5 +36,38 @@
     ?>
     </div>
   </div>
+</div>
+<div class="carousel product-slider">
+  <h4>New producks!</h4>
+<?php
+function output() {
+  echo "
+  <a class='carousel-item' href='#one!'>
+      <div class='card'>
+        <div class='card-image'>
+          <img src='http://via.placeholder.com/400x400'>
+          <span class='card-title'>Card Title</span>
+        </div>
+        <div class='card-action'>
+          <p class='price'>$99.95</p>
+          <div class='stars right'>
+            <i class='material-icons tiny rated'>star</i>
+            <i class='material-icons tiny rated'>star</i>
+            <i class='material-icons tiny rated'>star</i>
+            <i class='material-icons tiny rated'>star</i>
+            <i class='material-icons tiny'>star_border</i>
+          </div>
+        </div>
+      </div>
+  </a>
+  ";
+}
+
+$i = 1;
+while ($i <= 7) {
+  output();
+  $i++;
+}
+?>
 </div>
 <?php require_once('includes/footer.php') ?>
