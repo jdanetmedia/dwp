868227ae4dda84db801e1220ffdee56a91379889
Jesper Dalsgaard <post@jdanet.dk>
2018-03-06 09:33:33 +0100

    Added breadcrumbs and related products to blogpost

--- post.php
+++ post.php
@@ -1,5 +1,14 @@
 <?php require_once('includes/header.php') ?>
   <div class="container post-container">
+      <nav class="breadcrumb-nav">
+        <div class="nav-wrapper">
+          <div class="col s12">
+            <a href="#!" class="breadcrumb">Bluck</a>
+            <a href="#!" class="breadcrumb">Some category</a>
+            <a href="#!" class="breadcrumb">Post title</a>
+          </div>
+        </div>
+      </nav>
       <div class="row">
         <div class="col s12">
           <div class="card">
@@ -8,13 +17,39 @@
               <span class="card-title">Post title</span>
             </div>
             <div class="card-content">
-              <i>Posted on <b>18/2 2018</b> by <b>Jesper</b></i>
+              <i>Posted on <b>18/2 2018</b> by <b>Jesper</b> in <b>Some category</b></i>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. lorem</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </div>
         </div>
+      </div>
+      <div class="carousel product-slider">
+  			<h4>Related producks!</h4>
+  			<?php
+  			function output() {
+  			echo "
+  			<a class='carousel-item' href='product.php'>
+  			    <div class='card'>
+  			      <div class='card-image'>
+  			        <img src='http://via.placeholder.com/400x400'>
+  			        <span class='card-title'>Card Title</span>
+  			      </div>
+  			      <div class='card-action'>
+  			        <p class='price'>$99.95</p>
+  			      </div>
+  			    </div>
+  			</a>
+  			";
+  			}
+
+  			$i = 1;
+  			while ($i <= 7) {
+  			output();
+  			$i++;
+  			}
+  			?>
       </div>
   </div>
 <?php require_once('includes/footer.php') ?>
