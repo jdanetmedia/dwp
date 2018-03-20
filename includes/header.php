77c2f99c3da7d46d10123ba95d56234c4e4162b8
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-03-20 10:02:26 +0100

    Added more validation to newuser.php + removed noUIslider folder and imports

--- includes/header.php
+++ includes/header.php
@@ -20,8 +20,6 @@ $pageInfo = getPageInfo();
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="css/custom/custom.css" />
-      <!--Import noUIslider-->
-      <link type="text/css" rel="stylesheet" href="nouislider/nouislider.css" />
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
--- newuser.php
+++ newuser.php
@@ -10,30 +10,28 @@ if (!empty($message)) {echo "<p>" . $message . "</p>";}
           <form class="col s12 m12" action="" method="post">
             <div class="row">
               <div class="input-field col s6">
-                <input id="first_name" type="text" class="validate">
-                <label for="first_name">First Name*</label>
+                <input id="first_name" name="firstName" type="text" class="validate" value="<?php if(isset($_POST['firstName'])) { echo $_POST['firstName']; } ?>">
+                <label for="first_name">First Name</label>
               </div>
               <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
-                <label for="last_name">Last Name*</label>
+                <input id="last_name" name="lastName" type="text" class="validate" value="<?php if(isset($_POST['firstName'])) { echo $_POST['lastName']; } ?>">
+                <label for="last_name">Last Name</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input id="email" type="email" name="CustomerEmail" maxlength="100" class="validate">
+                <input id="email" type="email" name="CustomerEmail" maxlength="100" class="validate" value="<?php if(isset($_POST['firstName'])) { echo $_POST['CustomerEmail']; } ?>">
                 <label for="first_name">Email</label>
               </div>
             </div>
             <div class="row">
-              <div class="input-field col s12">
+              <div class="input-field col s12 m6">
                 <input id="password" type="password" name="pass" maxlength="30" class="validate">
                 <label for="password">Password</label>
               </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="password2" type="password" class="validate">
-                <label for="password">Repeat password*</label>
+              <div class="input-field col s12 m6">
+                <input id="password2" type="password" name="passvali" class="validate">
+                <label for="password">Repeat password</label>
               </div>
             </div>
             <input class="waves-effect waves-light btn" type="submit" name="submitcreateuser" value="Create">
