ede0dd8f8a84d6a1200760c5e853fbd02c7eaed9
Jesper Dalsgaard <post@jdanet.dk>
2018-04-17 10:56:17 +0200

    Startet OOP in admin area

---
+++ admin/class/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+class Product {
+  private $productName;
+  private $productCategoryID;
+  private $productStatus;
+  private $shortDescription;
+  private $longDescription;
+  private $price;
+  private $offerPrice;
+  private $stockStatus;
+  private $seoTitel;
+  private $metaDescription;
+
+
+}
