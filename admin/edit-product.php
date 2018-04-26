e7800165279d279c57fdf83704b4f72aa50b61f2
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-04-26 11:01:43 +0200

    more lockdown

--- admin/edit-product.php
+++ admin/edit-product.php
@@ -1,5 +1,15 @@
 <?php
 require_once("../admin/includes/header.php");
+
+if (!logged_in()) {
+?>
+<script type="text/javascript">
+	window.location.href = 'login.php';
+</script>
+<?php
+	//redirect_to("login.php");
+}
+
 $products = new Product();
 if(isset($_POST["submit"])) {
   $products->updateProduct($_GET["item"]);
