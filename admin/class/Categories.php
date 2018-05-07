60ba4b1ded04b0007974b7bc0bc8423587f808b3
Sebastian Buch <seba5513@easv365.dk>
2018-05-07 11:24:59 +0200

    Startet på kategori siden

---
+++ admin/class/Categories.php
@@ -0,0 +1,4 @@
+<?php
+Class Categories extends DBConnect {
+
+}
---
+++ admin/manage-categories.php
@@ -0,0 +1,120 @@
+<?php require_once("../admin/includes/header.php");
+spl_autoload_register(function($class) {
+    include "class/".$class.".php";
+});
+
+if (!logged_in()) {
+    ?>
+    <script type="text/javascript">
+        window.location.href = 'login.php';
+    </script>
+    <?php
+    //redirect_to("login.php");
+}
+$blogPosts = new BlogPosts();
+$allBlogPosts = $blogPosts->getAllBlogPosts();
+$allCategories = $blogPosts->getAllCategories();
+?>
+<div class="container">
+    <div class="row">
+    <div class="col s12">
+    <div class="card">
+        <div class="card-content">
+            <span class="card-title">Categories</span>
+        </div>
+        <div class="card-tabs">
+            <ul class="tabs tabs-fixed-width">
+                <li class="tab"><a href="#productCategories">Manage product categories</a></li>
+                <li class="tab"><a href="#blogPostCategories">Manage blog post categories</a></li>
+            </ul>
+        </div>
+        <div class="card-content grey lighten-4">
+            <div class="row" id="productCategories">
+                <div class="col s12">
+                    <div class="card">
+                        <div class="card-content">
+                            <span class="card-title">Product Categories<a class="waves-effect waves-light btn grey darken-4 new-prod-btn" href="new-blogpost.php">Add new</a></span>
+                            <table class="responsive-table striped">
+                                <thead>
+                                <tr>
+                                    <th>Post date</th>
+                                    <th>Title</th>
+                                    <th>Blog content</th>
+                                    <th>Category</th>
+                                    <th>Author</th>
+                                    <th>Edit</th>
+                                </tr>
+                                </thead>
+                                <?php // TODO: Ændre farve på select felter ?>
+                                <tbody>
+                                <?php
+                                foreach ($allBlogPosts as $aBlogPost) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $aBlogPost->BlogDate; ?></td>
+                                        <td><?php echo $aBlogPost->Title; ?></td>
+                                        <td><?php if (strlen($aBlogPost->BlogContent) > 100) {
+                                                echo preg_replace('/\s+?(\S+)?$/', '', substr
+                                                    ($aBlogPost->BlogContent, 0, 100)) . " ...";
+                                            } else echo $aBlogPost->BlogContent; ?></td>
+                                        <td><?php echo $aBlogPost->CategoryName; ?></td>
+                                        <td><?php echo $aBlogPost->UserEmail; ?></td>
+                                        <td><a href="edit-blog-post.php?ID=<?php echo $aBlogPost->BlogPostID; ?>">Edit</a></td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="blogPostCategories">
+                <div class="col s12">
+                    <div class="card">
+                        <div class="card-content">
+                            <span class="card-title">Blog Post Categories<a class="waves-effect waves-light btn grey darken-4 new-prod-btn" href="new-blogpost.php">Add new</a></span>
+                            <table class="responsive-table striped">
+                                <thead>
+                                <tr>
+                                    <th>Post date</th>
+                                    <th>Title</th>
+                                    <th>Blog content</th>
+                                    <th>Category</th>
+                                    <th>Author</th>
+                                    <th>Edit</th>
+                                </tr>
+                                </thead>
+                                <?php // TODO: Ændre farve på select felter ?>
+                                <tbody>
+                                <?php
+                                foreach ($allBlogPosts as $aBlogPost) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $aBlogPost->BlogDate; ?></td>
+                                        <td><?php echo $aBlogPost->Title; ?></td>
+                                        <td><?php if (strlen($aBlogPost->BlogContent) > 100) {
+                                                echo preg_replace('/\s+?(\S+)?$/', '', substr
+                                                    ($aBlogPost->BlogContent, 0, 100)) . " ...";
+                                            } else echo $aBlogPost->BlogContent; ?></td>
+                                        <td><?php echo $aBlogPost->CategoryName; ?></td>
+                                        <td><?php echo $aBlogPost->UserEmail; ?></td>
+                                        <td><a href="edit-blog-post.php?ID=<?php echo $aBlogPost->BlogPostID; ?>">Edit</a></td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
+
+<?php require_once("../admin/includes/footer.php"); ?>
