e7e50aa23f85a55839aafeb688ad52c6e13c367f
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-05-09 08:47:42 +0200

    Start on profile page

---
+++ view/profile.php
@@ -0,0 +1,59 @@
+<?php
+require_once("../includes/sessionstart.php");
+require_once("../includes/header.php");
+//$Customerinfo = getCustomerInfo($_SESSION["CustomerEmail"]);
+
+if (!logged_in()) {
+?>
+<script type="text/javascript">
+	window.location.href = 'login.php';
+</script>
+<?php
+	//redirect_to("login.php");
+}
+?>
+<div class="container">
+  <h1>Profile</h1>
+  <div class="row">
+    <form class="col s12" action="" method="post">
+      <div class="row">
+        <div class="input-field col s6">
+          <input value="Name" id="first_name" type="text" class="validate">
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input value="Name" id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Old password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Repeat password</label>
+        </div>
+      </div>
+      <input class="waves-effect waves-light btn cart-btt right" type="submit" name="updateprofile" value="Update">
+    </form>
+  </div>
+</div>
+<?php
+require_once("../includes/footer.php");
+?>
