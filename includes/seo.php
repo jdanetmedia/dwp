fc86d7ac08a8bebb75373f5c4ff2de970864e6ee
Jesper Dalsgaard <post@jdanet.dk>
2018-05-11 13:09:27 +0200

    SEO for productcategories and products page

--- includes/seo.php
+++ includes/seo.php
@@ -13,7 +13,7 @@
         <?php
     } else {
       ?>
-        <title><?php echo $productInfo["ProuctName"]; ?></title>
+        <title><?php echo $productInfo["ProductName"]; ?></title>
       <?php
     }
     // Meta description
@@ -31,8 +31,28 @@
 
   $basename = substr(strtolower(basename($_SERVER['PHP_SELF'])),0,strlen(basename($_SERVER['PHP_SELF']))-4);
 
-  // Products page
-  if($basename == "product" && !isset($_GET["item"]) && !isset($_GET["cat"]) || isset($_GET["cat"]) && $_GET["cat"] == 0) {
-    print_r($basename);
+  // Products page and product categories
+  if($basename == "products" && !isset($_GET["item"]) && !isset($_GET["cat"]) || $basename == "products" && $_GET["cat"] == 0) {
+    ?>
+      <title><?php echo $basename; ?></title>
+    <?php
+  } elseif($basename == "products" && !isset($_GET["item"]) && isset($_GET["cat"]) && $_GET["cat"] != 0) {
+    include("../admin/class/DBConnect.php");
+    include("../admin/class/Categories.php");
+    $categories = new Categories();
+    $currentCat = $categories->getProductCategoryDetails($_GET["cat"]);
+    if(isset($currentCat[0]["SeoTitle"]) && !empty($currentCat[0]["SeoTitle"])) {
+      ?>
+        <title><?php echo $currentCat[0]["SeoTitle"]; ?></title>
+      <?php
+      ?>
+        <meta name="description" content="<?php echo $currentCat[0]["MetaDescription"]; ?>">
+      <?php
+    } else {
+      ?>
+        <title><?php echo $currentCat[0]["CategoryName"]; ?></title>
+      <?php
+    }
   }
+
 ?>
