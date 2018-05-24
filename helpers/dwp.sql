3d6632a95edb94f8fa56f98e81205205220ae8c3
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2018-05-24 17:53:21 +0200

    Changes to SQL script

--- helpers/dwp.sql
+++ helpers/dwp.sql
@@ -263,7 +263,7 @@ INSERT INTO User
 VALUES ('rasmus.andreas96@gmail.com', '$2a$10$74zsjq9/Tv6Ydq.QLlKeju.bwxXfs8GUSN051E1EeMIi4L/beo1Li', 'Rasmus Andreas', 'Nielsen', NULL, 1);
 
 INSERT INTO User
-VALUES ('admin@admin.com', '$2a$10$74zsjq9/Tv6Ydq.QLlKeju.bwxXfs8GUSN051E1EeMIi4L/beo1Li', 'Admin', 'Admin', NULL, 1);
+VALUES ('admin@dwp.com', '$2y$10$8esR6xSS.3XBOC3trjPXS.yZJsQluApvQsoyHeJOLbshZsdkFFU5a', 'Admin', 'Admin', NULL, 1);
 
 INSERT INTO User
 VALUES ('post@jdanet.dk', '$2a$10$74zsjq9/Tv6Ydq.QLlKeju.bwxXfs8GUSN051E1EeMIi4L/beo1Li', 'Jesper', 'Dalsgaard', NULL, 0);
@@ -1812,7 +1812,7 @@ INSERT INTO Customer
 VALUES ('rasmus.andreas96@gmail.com', '$2a$10$74zsjq9/Tv6Ydq.QLlKeju.bwxXfs8GUSN051E1EeMIi4L/beo1Li', 'Spangsbjerg Kirkevej','99B', NULL, 'Rasmus Andreas', 'Nielsen', '6700', NULL);
 
 INSERT INTO Customer
-VALUES ('sebastiankbuch@hotmail.com', '$2a$10$QMmK4nTUF6szqmNG3t8V/uTu5BM7ejLvaRSN7aUFoxJY4hsvzYBhO', NULL, NULL, NULL, 'Sebastian', 'Buch', NULL, NULL);
+VALUES ('kunde@dwp.com', '$2y$10$8esR6xSS.3XBOC3trjPXS.yZJsQluApvQsoyHeJOLbshZsdkFFU5a', NULL, NULL, NULL, 'Kunde', 'Bruger', NULL, NULL);
 
 INSERT INTO Customer
 VALUES ('post@jdanet.dk', '$2a$10$QMmK4nTUF6szqmNG3t8V/uTu5BM7ejLvaRSN7aUFoxJY4hsvzYBhO', NULL, NULL, NULL, 'Jesper', 'Dalsgaard', NULL, NULL);
@@ -1854,10 +1854,10 @@ INSERT INTO CustomerOrder
 VALUES (NULL, 'This is a order comment', 'testChargeID', '2008-11-11 13:23:44', 'Nørregade', '30', 6700, 'rasmus.andreas96@gmail.com', 3, 1, NULL);
 
 INSERT INTO CustomerOrder
-VALUES (NULL, 'This is a order comment', 'testChargeID', '2010-11-11 13:23:44', 'Spangsbjerg Kirkevej', '99B, 16', 6700, 'sebastiankbuch@hotmail.com', 1, 2, NULL);
+VALUES (NULL, 'This is a order comment', 'testChargeID', '2010-11-11 13:23:44', 'Spangsbjerg Kirkevej', '99B, 16', 6700, 'kunde@dwp.com', 1, 2, NULL);
 
 INSERT INTO CustomerOrder
-VALUES (NULL, 'This is a order comment', 'testChargeID', '2005-11-11 13:23:44', 'Nørregade', '30', 6700, 'sebastiankbuch@hotmail.com', 3, 1, NULL);
+VALUES (NULL, 'This is a order comment', 'testChargeID', '2005-11-11 13:23:44', 'Nørregade', '30', 6700, 'kunde@dwp.com', 3, 1, NULL);
 
 INSERT INTO CustomerOrder
 VALUES (NULL, 'This is a order comment', 'testChargeID', '2012-11-11 13:23:44', 'Spangsbjerg Kirkevej', '99B, 16', 6700, 'post@jdanet.dk', 1, 2, NULL);
